5b62df586178f7af23e876f1bfca07be87af1260
biak <bsprac@gmail.com>
2015-03-05 19:17:45 +0530

    Update server.js

--- tweet/server.js
+++ tweet/server.js
@@ -2,15 +2,16 @@ var path = require('path');
 var express = require('express');
 var app = express();
 var Twitter = require('twit');
-var dbCon = require('mongojs').connect('mongodb://biak:biak123@ds061558.mongolab.com:61558/clienttable');
+var dbCon = require('mongojs').connect('mongodb://bk:bk123@ds061558.mongolab.com:61558/clienttable');
 var db = dbCon.collection('tweet');
-app.use(express.static(path.join(__dirname, 'public'))); 
+
+app.use(express.static(path.join(__dirname, 'publictweet'))); 
 
 var client = new Twitter({
   consumer_key: '9wKcujVrvjQVAnvbojfGNLoGY',
   consumer_secret: 'VgjFAhh5f0EtzMHcOCRTwuPFtIV6jB9VXiCSawdviGCFo5ATmG',
-  access_token: ' 2997059396-8xr9NHG3rAJaZnUOxt1LSWdXqbP8Yhim2h5xXbd',
-  access_token_secret: ' kSDhkzwi3W6ltgsHHHCbpLZgCJnOr8k8mcrLp8BlPAuml'
+  access_token: '2997059396-8xr9NHG3rAJaZnUOxt1LSWdXqbP8Yhim2h5xXbd',
+  access_token_secret: 'kSDhkzwi3W6ltgsHHHCbpLZgCJnOr8k8mcrLp8BlPAuml'
   /*,request_options: {
     proxy: 'http://myproxyserver.com:1234'
   }*/
@@ -31,9 +32,10 @@ client.get('favorites/list', function(error, tweets, response){
 		console.log(docs);
 		res.json(docs);
 	});
-    console.log('Done');
+    console.log('sadasdas');
    
 });
 	
 });
-app.listen(process.env.PORT || 5000);
+app.listen(3000);
+
